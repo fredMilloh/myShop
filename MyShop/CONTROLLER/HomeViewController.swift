e6981ab8edb95eca725d8a1c6f7c6466c1ab2289
fredMilloh <fred.milloh@hotmail.com>
2020-09-24 18:46:00 +0200

    start version tab bar + 4 controller

---
+++ MyShop/CONTROLLER/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  MyShop
+//
+//  Created by fred on 24/09/2020.
+//  Copyright © 2020 fred. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
