5f3e7b1979fc41ca0c89d6b9ecd9379be2ed1b95
fredMilloh <fred.milloh@hotmail.com>
2020-11-06 12:09:24 +0100

    hideBarOnSwippe

--- MyShop/CONTROLLER/SearchResultTVController.swift
+++ MyShop/CONTROLLER/SearchResultTVController.swift
@@ -19,8 +19,6 @@ class SearchResultTVController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     func searchBar () {
